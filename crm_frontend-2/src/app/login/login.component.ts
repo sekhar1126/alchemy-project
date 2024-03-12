ff41f0371fb8a90d05a83ecac9b8aec65f54a45a
sekhar1126 <sekharkumpatla01@gmail.com>
2024-03-12 18:01:01 +0530

    Front end

---
+++ crm_frontend-2/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: any;
+  onSubmit() {
+  throw new Error('Method not implemented.');
+  }
+}
